9cd810d1964a46a9c353cd6418a4dbb5679f6fce
hongxiaorong <24320152202743@stu.xmu.edu.cn>
2017-12-20 21:21:24 +0800

    mybatis 配置again

--- src/main/java/xmu/crms/ClasssystemApplication.java
+++ src/main/java/xmu/crms/ClasssystemApplication.java
@@ -1,9 +1,11 @@
 package xmu.crms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages="xmu.crms.mapper")
 public class ClasssystemApplication {
 public static void main(String[] args)
 {
--- src/main/java/xmu/crms/mapper/ClassMapper.java
+++ src/main/java/xmu/crms/mapper/ClassMapper.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.math.BigInteger;
 
 @Component
-@Mapper
+
 public interface ClassMapper {
 
     Boolean deleteClassSelectionByClassId(BigInteger classId);
