534f80ee1dd0e08e61086d85af44e3fa1ce5406a
hongxiaorong <24320152202743@stu.xmu.edu.cn>
2017-12-20 21:16:50 +0800

    Revert "Revert "配置mybatis""
    This reverts commit 0b0a372e8c50129a7a69da1589b3de3526f25df8.

--- src/main/java/xmu/crms/dao/Impl/ClassDaoImpl.java
+++ src/main/java/xmu/crms/dao/Impl/ClassDaoImpl.java
@@ -1,10 +1,21 @@
 package xmu.crms.dao.Impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import xmu.crms.dao.ClassDao;
+import xmu.crms.mapper.ClassMapper;
+
+import java.math.BigInteger;
 
 @Component
 public class ClassDaoImpl implements ClassDao{
 
+    @Autowired
+    private ClassMapper classMapper;
+    @Override
+    public Boolean deleteClassSelectionByClassId(BigInteger classId) {
+        classMapper.deleteClassSelectionByClassId(classId);
 
+        return null;
+    }
 }
