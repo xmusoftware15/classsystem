9b681275408e0bef220a1e80c819d409f60f4057
Tracy-Mac <34231282+Tracy-Mac@users.noreply.github.com>
2017-12-23 02:36:18 +0800

    解决冲突

--- src/main/java/xmu/crms/service/Impl/CourseServiceImpl.java
+++ src/main/java/xmu/crms/service/Impl/CourseServiceImpl.java
@@ -1,47 +1,65 @@
 package xmu.crms.service.Impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import xmu.crms.dao.CourseDao;
 import xmu.crms.entity.ClassInfo;
 import xmu.crms.entity.Course;
 import xmu.crms.exception.CourseNotFoundException;
+import xmu.crms.service.ClassService;
 import xmu.crms.service.CourseService;
 
 import java.math.BigInteger;
 import java.util.List;
 
+@Service
 public class CourseServiceImpl implements CourseService{
+
+    @Autowired
+    private CourseDao courseDao;
+
+    @Autowired
+    private ClassService classService;
+
     @Override
     public List<Course> listCourseByUserId(BigInteger userId) throws IllegalArgumentException, CourseNotFoundException {
-        return null;
+        return courseDao.listCourseByUserId(userId);
     }
 
     @Override
     public BigInteger insertCourseByUserId(BigInteger userId, Course course) throws IllegalArgumentException {
-        return null;
+        return courseDao.insertCourseByUserId(userId, course);
     }
 
     @Override
     public Course getCourseByCourseId(BigInteger courseId) throws IllegalArgumentException, CourseNotFoundException {
-        return null;
+        return courseDao.getCourseByCourseId(courseId);
     }
 
     @Override
     public void updateCourseByCourseId(BigInteger courseId, Course course) {
-
+        courseDao.updateCourseByCourseId(courseId, course);
     }
 
     @Override
-    public void deleteCourseByCourseId(BigInteger courseId) throws IllegalArgumentException, CourseNotFoundException {
-
+    public void deleteCourseByCourseId(BigInteger courseId) throws IllegalArgumentException{
+        courseDao.deleteCourseByCourseId(courseId);
     }
 
     @Override
     public List<Course> listCourseByCourseName(String courseName) {
-        return null;
+        return courseDao.listCourseByCourseName(courseName);
     }
 
     @Override
     public List<ClassInfo> listClassByCourseName(String courseName) {
-        return null;
+        List<Course> courses = listCourseByCourseName(courseName);
+        List<ClassInfo> classInfos;
+        for(Course course : courses){
+            classService.listClassByCourseId(course.getId())
+        }
     }
 
     @Override
@@ -50,7 +68,7 @@ public class CourseServiceImpl implements CourseService{
     }
 
     @Override
-    public List<ClassInfo> listClassByUserId(BigInteger userId) throws IllegalArgumentException, CourseNotFoundException, ClassNotFoundException {
+    public List<ClassInfo> listClassByUserId(BigInteger userId) throws IllegalArgumentException, ClassNotFoundException {
         return null;
     }
 }
